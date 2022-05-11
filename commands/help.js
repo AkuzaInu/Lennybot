554335015d88d8c1cd823d8610c104b05b132dc0
AkuzaInu <100430402+AkuzaInu@users.noreply.github.com>
2022-05-11 16:18:30 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
 		.setDescription(`Type \`${prefix}lenny\` to access all lenny's commands.`)
 		.addFields(
 			{name: `${prefix}help`, value: `Opens the help panel. *(The one you're looking at right now)*`},
-			{name: `Source code`, value: `[Github](https://github.com/AkuzaInu/Discord/tree/main/Lennybot)`},
+			{name: `Source code`, value: `[Github](https://github.com/AkuzaInu/Lennybot)`},
 			{name: `Want me in your server?`, value: `[Invite me](https://discord.com/api/oauth2/authorize?client_id=973693504544964718&permissions=8&scope=bot)`},
 			{name: `Types`, value: `\`${types.join('\`, \`')}\``}
 		)
